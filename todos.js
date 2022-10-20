db85797407acb3229da5d8f4dbff80cdc5723444
Richard So <dev.richard.so@gmail.com>
2022-10-20 20:17:57 +1300

    add list of to dos

---
+++ todos.js
@@ -0,0 +1,31 @@
+// how are we going to group ourselves?
+
+// list of to dos...
+
+//  initial setup
+
+// 1. npm init
+// 2. install express and create index and server js
+// 3. set package json to have script to have start: node index.js
+// 4. install handlebars
+// 5. configure handlebars and engine to the server
+// 5a. create gitignore
+
+// division of work from here...
+
+// 6. create main.hbs (html) and create and set up other handlebars views pages and formate {{}} these according to data.json structure
+// 7. we need to have main.hbs, home.hbs detail.hbs
+// 7a. create main.css
+
+// 9. create a function to load data.json  - readFile
+// 10. create a route for home route
+// 11. create a route for detail route
+
+// *** - features to add if possible. - *** //
+// *** 8. detail.hbs will need to have a form that submits star ratings and  users comments
+// *** 8a. detail.hbs needs to have a section where it can hold users comments in the format of <li>"users comments"</li> ??
+// *** 12. create a route for POST for user comments. - writeFile
+
+// star ratings. <- based on users rating. push them all to the array and average out.
+// comments
+// mouse over
