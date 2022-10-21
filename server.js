4c5a0b9a5c4f0b8e6d10f17d04c65dc33a173076
shymala <shymala.sidharth22@gmail.com>
2022-10-21 13:04:42 +1300

    add CSS homepage

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const server = express()
+//path setup
+const path = require('path')
 
 // Server configuration
 server.use(express.static('public'))
@@ -8,9 +10,6 @@ server.use(express.urlencoded({ extended: false }))
 
 // Handlebars configuration
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
-
-//path setup
-const path = require('path')
 
 //require fs
 const fs = require('node:fs/promises')
@@ -25,7 +24,6 @@ server.get('/', (req, res) => {
   const filepath = path.join(__dirname, 'data.json')
   fs.readFile(filepath, 'utf-8')
     .then((data) => {
-      console.log(typeof data)
       const viewData = JSON.parse(data)
       res.render('home', viewData)
     })
