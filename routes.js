f0f4255dba30d4d046a1dda8e73654187d0be7c8
Richard So <dev.richard.so@gmail.com>
2022-10-21 14:31:10 +1300

    add comments

--- routes.js
+++ routes.js
@@ -4,8 +4,9 @@ module.exports = router
 const fs = require('node:fs/promises')
 const path = require('path')
 
+const filePath = path.join(__dirname, 'data.json')
+
 router.get('/:id', (req, res) => {
-  const filePath = path.join(__dirname, 'data.json')
   fs.readFile(filePath, 'utf-8')
     .then((data) => {
       // console.log(data)
@@ -19,17 +20,27 @@ router.get('/:id', (req, res) => {
     })
 })
 
-// router.get('/:id/edit', (req, res) => {
-//   const filePath = path.join(__dirname, 'data.json')
-//   fs.readFile(filePath, 'utf-8')
-//     .then((data) => {
-//       const viewData = JSON.parse(data)
-//       const newData = viewData.movies.find((info) => info.id === +req.params.id)
-//       console.log(newData)
-//       res.render('edit', newData)
-//     })
-//     .catch((err) => {
-//       console.error(err)
-//     })
-// })
-//res.se
+router.post('/:id', async (req, res) => {
+  const movieId = req.params.id
+  const newComment = `${req.body.name}: ${req.body.comment}`
+  try {
+    const data = await fs.readFile(filePath, 'utf-8')
+    const jsonData = JSON.parse(data)
+    let selectedMovie = jsonData.movies.find((el) => el.id == movieId)
+    const remainingMovie = jsonData.movies.filter((el) => el.id != movieId)
+    selectedMovie.comments.push(newComment)
+    const newObj = {
+      movies: [...remainingMovie],
+    }
+    newObj.movies.push(selectedMovie)
+    const dataStringify = JSON.stringify(newObj, null, 2)
+    try {
+      await fs.writeFile(filePath, dataStringify, 'utf-8')
+    } catch (error) {
+      console.log(error.message)
+    }
+    res.redirect(`/movies/${movieId}`)
+  } catch (error) {
+    console.log(error.message)
+  }
+})
